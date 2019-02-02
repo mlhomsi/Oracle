eb54c3b4805943633d591954e1f6a0340bd91455
mlhomsi <mlhomsi@gmail.com>
2019-02-02 04:19:59 +0000

    Working Test Level and more basic Navigation functions

---
+++ Oracle/Assets/Rebaker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Rebaker : MonoBehaviour
+{
+    public NavMeshSurface Surface;
+
+    void Start()
+    {
+        Surface.BuildNavMesh();
+    }
+}
