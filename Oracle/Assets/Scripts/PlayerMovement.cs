6deae17e7c7463fc115dd8e6d8e6e7661a0f5541
mlhomsi <mlhomsi@gmail.com>
2019-02-02 01:28:11 +0000

    Folder Organization

---
+++ Oracle/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera Cam;
+    public NavMeshAgent Agent;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray Ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+
+            if(Physics.Raycast(Ray, out Hit))
+            {
+                Agent.SetDestination(Hit.point);
+            }
+        }
+    }
+}
