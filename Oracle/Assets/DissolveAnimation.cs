0396ca35be0719c1a3f346743d5a2bf886e12e75
mlhomsi <mlhomsi@gmail.com>
2019-02-04 01:41:44 -0200

    Cleanup & Shader Test

---
+++ Oracle/Assets/DissolveAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class DissolveAnimation : MonoBehaviour
+{
+    public GameObject ThisDoor; //This Object
+
+    public bool IsDissolving = false; // Vanish Trigger
+    public Material Door; //Material with the Vanish Shader
+    private float CurrentStage;
+
+    public NavMeshSurface Surface;
+
+
+
+    private void Start()
+    {
+        //Set variable "Stage Of Dissolve" to -0,1f, it´s minimum value;
+        Door.SetFloat("Vector1_5658DA2C", -0.1f);
+    }
+    void Update()
+    {
+        //Get the current Stage Of Dissolve; if greater than 0.5f, disables the object´s collider;
+        CurrentStage = Door.GetFloat(("Vector1_5658DA2C"));
+        if (CurrentStage >= 0.5f)
+        {
+            ThisDoor.GetComponent<BoxCollider>().enabled = false;
+            ThisDoor.layer = 8;
+            Surface.BuildNavMesh();
+        }
+    }
+    public void TriggerDissolve()
+    {
+        IsDissolving = true;
+        StartCoroutine(Dissolve());
+    }
+    IEnumerator Dissolve()
+    {
+        for (float f = 0f; f <= 1.1f; f += 0.01f)
+        {
+            {
+                Door.SetFloat("Vector1_5658DA2C", f);
+                //Debug.Log(f);
+                yield return new WaitForSeconds(0.001f);
+            }
+        }
+    }
+    
+}
---
+++ Oracle/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera Cam;
+    public NavMeshAgent Agent;
+
+   // public ThirdPersonCharacter Char;
+
+    private void Start()
+    {
+      //  Agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray Ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+
+            if(Physics.Raycast(Ray, out Hit))
+            {
+                Agent.SetDestination(Hit.point);
+            }
+        }
+
+        if(Agent.remainingDistance > Agent.stoppingDistance)
+        {
+           // Char.Move(Agent.desiredVelocity, false, false);
+        }
+       // else Char.Move(Vector3.zero, false, false);
+
+    }
+}
