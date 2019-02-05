d3e6925d9e00224aaa51c0ef611060ad5cb4fece
Matheus Homsi <mlhomsi@gmail.com>
2019-02-05 02:39:57 -0200

    Patrolling Tests

---
+++ Oracle/Assets/FOV.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    public GameObject player;
+    private Transform Startposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Startposition = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            player.transform.position = Startposition.position;
+        }
+    }
+}
--- Oracle/Assets/Scripts/Hover.cs
+++ Oracle/Assets/Scripts/Hover.cs
@@ -6,10 +6,12 @@ public class Hover : MonoBehaviour
 {
     public Material Outline;
     public float OutlineThinness = 0.7f;
+    //public GameObject button;
 
     // Start is called before the first frame update
     void Start()
     {
+        //button.SetActive(false);
         Outline.SetFloat("Vector1_CFE0059", 1.1f);
 
     }
@@ -23,11 +25,13 @@ public class Hover : MonoBehaviour
     private void OnMouseOver()
     {
         Outline.SetFloat("Vector1_CFE0059", OutlineThinness);
+        //button.SetActive(true);
     }
 
     private void OnMouseExit()
     {
         Outline.SetFloat("Vector1_CFE0059", 1.1f);
+        //button.SetActive(false);
 
     }
 }
