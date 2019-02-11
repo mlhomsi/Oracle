9da14f4370d35fcefb5983bf46423d3981094f95
mlhomsi <mlhomsi@gmail.com>
2019-02-11 00:28:55 -0200

    Wrap Up

--- Oracle/Assets/Scripts/Hover.cs
+++ Oracle/Assets/Scripts/Hover.cs
@@ -8,14 +8,16 @@ public class Hover : MonoBehaviour
     public Material Outline;
     public float OutlineThinness = 0.7f;
     public GameObject button;
+    public GameObject light;
 
-    public Button ActionButton;
+    //public Button ActionButton;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        //button.SetActive(false);
+        button.SetActive(false);
+        light.SetActive(false);
         Outline.SetFloat("Vector1_CFE0059", 1.1f);
 
     }
@@ -32,6 +34,8 @@ public class Hover : MonoBehaviour
         //button.SetActive(true);
         //ActionButton.transform.Translate(Input.mousePosition);
         button.SetActive(true);
+        light.SetActive(true);
+
     }
 
     private void OnMouseExit()
@@ -39,6 +43,8 @@ public class Hover : MonoBehaviour
         Outline.SetFloat("Vector1_CFE0059", 1.1f);
         //button.SetActive(false);
         button.SetActive(false);
+        light.SetActive(false);
+
 
     }
 }
